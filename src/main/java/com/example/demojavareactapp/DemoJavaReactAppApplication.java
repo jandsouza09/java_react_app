9b5530aa92a918c5354c6272560ac8c105f6a194
Janice Dsouza <janice.dsouza@nielseniq.com>
2024-08-13 23:24:36 -0400

    Initial commit

---
+++ src/main/java/com/example/demojavareactapp/DemoJavaReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavareactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaReactAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJavaReactAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojavareactapp/DemoJavaReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojavareactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaReactAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
